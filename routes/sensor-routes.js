b221b467836fed2e4e3e985e738e879aa88b9215
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-20 10:19:29 +0530

    Implemented nodemailer code to send emails when co2 and smoke levels increased

--- routes/sensor-routes.js
+++ routes/sensor-routes.js
@@ -4,6 +4,14 @@ const Sensor = require("../models/sensor-model");
 const nodeailer = require("nodemailer");
 require("dotenv").config();
 
+let transporter = nodeailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "ayeshlak1998@gmail.com",
+    pass: process.env.PASSWORD,
+  },
+});
+
 router.post("/sensor", (req, res, next) => {
   Sensor.create(req.body)
     .then((sensor) => {
@@ -18,20 +26,46 @@ router.put("/sensor/:id", (req, res, next) => {
     { $set: req.body },
     { new: true },
     (error, doc) => {
-      if (doc.smokeLevel > 5) {
-        let transporter = nodeailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: "ayeshlak1998@gmail.com",
-            pass: process.env.PASSWORD,
-          },
+      if (doc.co2Level > 5 && doc.smokeLevel > 5) {
+        let info = {
+          from: '"Sensor Alerts 💥🔥" ayeshlak1998@gmail.com',
+          to: "aruniprashani@gmail.com",
+          subject: "SmokeLevel and CO2 level Increased",
+          text: `SmokeLevel of the sensor ${doc.id} increased to ${doc.smokeLevel}  and CO2 Level of the sensor ${doc.id} increased to ${doc.co2Level}`,
+        };
+
+        transporter.sendMail(info, (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("sent");
+          }
         });
 
+        console.log(doc.co2Level);
+      } else if (doc.co2Level > 5) {
+        let info = {
+          from: '"Sensor Alerts 💥🔥" ayeshlak1998@gmail.com',
+          to: "aruniprashani@gmail.com",
+          subject: "CO2 Level Increased",
+          text: `CO2 Level of the sensor ${doc.id} increased to ${doc.co2Level}`,
+        };
+
+        transporter.sendMail(info, (err, data) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("sent");
+          }
+        });
+
+        console.log(doc.co2Level);
+      } else if (doc.smokeLevel > 5) {
         let info = {
           from: '"Sensor Alerts 💥🔥" ayeshlak1998@gmail.com',
           to: "aruniprashani@gmail.com",
           subject: "SmokeLevel Increased",
-          text: "SmokeLevel",
+          text: `SmokeLevel of the sensor ${doc.id} increased to ${doc.smokeLevel}`,
         };
 
         transporter.sendMail(info, (err, data) => {
@@ -43,10 +77,6 @@ router.put("/sensor/:id", (req, res, next) => {
         });
 
         console.log(doc.smokeLevel);
-      } else if (doc.co2Level > 5) {
-        console.log(doc.co2Level);
-      } else if (doc.co2Level > 5 && doc.smokeLevel > 5) {
-        console.log(doc.co2Level);
       }
 
       res.send(doc);
