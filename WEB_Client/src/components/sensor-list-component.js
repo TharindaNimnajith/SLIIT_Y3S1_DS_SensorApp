cecca2536349ede23641093fcf1150f52f23ca44
Ayesh1998 <ayeshlak1998@gmail.com>
2020-05-05 08:40:25 +0530

    Changes in web_client

--- WEB_Client/src/components/sensor-list-component.js
+++ WEB_Client/src/components/sensor-list-component.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 // sensor list component
@@ -15,7 +15,7 @@ class SensorList extends Component {
           floorNo: 3,
           roomNo: 1,
           smokeLevel: 4,
-          co2Level: 6,
+          co2Level: 3,
         },
         {
           id: "4",
@@ -25,7 +25,7 @@ class SensorList extends Component {
           roomNo: 2,
           smokeLevel: 3,
           co2Level: 2,
-        }
+        },
       ],
     };
   }
@@ -35,7 +35,7 @@ class SensorList extends Component {
     axios
       .get("http://localhost:5000/api/sensor")
       .then((response) => {
-        this.setState({sensors: [...response.data]});
+        this.setState({ sensors: [...response.data] });
         console.log("response");
       })
       .catch((err) => {
@@ -56,24 +56,30 @@ class SensorList extends Component {
   // if co2 level or smoke level of a sensor is greater than 5, color that row in red
   render() {
     return (
-      <div>
-        <h1>Sensor Details</h1>
-        <table className="table table-bordered  ">
-          <thead className="thead-light">
-          <tr>
-            <th>Sensor ID</th>
-            <th>Sensor Name</th>
-            <th>Active Status</th>
-            <th>Floor Number</th>
-            <th>Room Number</th>
-            <th>Smoke Level (1-10)</th>
-            <th>CO2 Level (1-10)</th>
-          </tr>
+      <div className="container" style={{ marginTop: "50px" }}>
+        <h1 style={{ textAlign: "center", padding: "15px", color: "#343a40" }}>
+          Sensor Details
+        </h1>
+        <table className="table table-bordered">
+          <thead className="thead-dark">
+            <tr style={{ textAlign: "center" }}>
+              <th>Sensor ID</th>
+              <th>Sensor Name</th>
+              <th>Active Status</th>
+              <th>Floor Number</th>
+              <th>Room Number</th>
+              <th>Smoke Level (1-10)</th>
+              <th>CO2 Level (1-10)</th>
+            </tr>
           </thead>
           <tbody>
-          {this.state.sensors.map(sensor => {
-              return (sensor.smokeLevel > 5 || sensor.co2Level > 5) ? (
-                <tr bgcolor="#FF0000" style={{color: 'White'}} key={sensor.id}>
+            {this.state.sensors.map((sensor) => {
+              return sensor.smokeLevel > 5 || sensor.co2Level > 5 ? (
+                <tr
+                  bgcolor="#FF0000"
+                  style={{ color: "white" }}
+                  key={sensor.id}
+                >
                   <td>{sensor.id}</td>
                   <td>{sensor.name}</td>
                   <td>{sensor.active ? "Active" : "Not Active"}</td>
@@ -81,17 +87,19 @@ class SensorList extends Component {
                   <td>{sensor.roomNo}</td>
                   <td>{sensor.smokeLevel}</td>
                   <td>{sensor.co2Level}</td>
-                </tr>) : (<tr bgcolor='White' key={sensor.id}>
-                <td>{sensor.id}</td>
-                <td>{sensor.name}</td>
-                <td>{sensor.active ? "Active" : "Not Active"}</td>
-                <td>{sensor.floorNo}</td>
-                <td>{sensor.roomNo}</td>
-                <td>{sensor.smokeLevel}</td>
-                <td>{sensor.co2Level}</td>
-              </tr>)
-            }
-          )}
+                </tr>
+              ) : (
+                <tr bgcolor="White" key={sensor.id}>
+                  <td>{sensor.id}</td>
+                  <td>{sensor.name}</td>
+                  <td>{sensor.active ? "Active" : "Not Active"}</td>
+                  <td>{sensor.floorNo}</td>
+                  <td>{sensor.roomNo}</td>
+                  <td>{sensor.smokeLevel}</td>
+                  <td>{sensor.co2Level}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
